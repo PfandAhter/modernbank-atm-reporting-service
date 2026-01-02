47d38ef75ea132cc81985c201c494051f3ba5436
Ataberk Bakir <bakirr.ataberk@gmail.com>
2026-01-02 15:19:15 +0300

    Externalize Kafka bootstrap server URL to application properties
    - Replace hardcoded Kafka bootstrap server URLs in `KafkaConfiguration` with property injection through `@Value`.
    - Improve flexibility and maintainability by reading the server address from application properties.

--- src/main/java/com/modernbank/atm_reporting_service/config/KafkaConfiguration.java
+++ src/main/java/com/modernbank/atm_reporting_service/config/KafkaConfiguration.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -22,10 +23,13 @@ import java.util.Map;
 @Slf4j
 public class KafkaConfiguration {
 
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String kafkaBootstrapServers;
+
     @Bean
     public ProducerFactory<String, CreatePDFRequest> pdfProducerFactory(){
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // Kafka broker adresi
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers); // Kafka broker adresi
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, true);
@@ -42,7 +46,7 @@ public class KafkaConfiguration {
     @Bean
     public ConsumerFactory<String, CreatePDFRequest> pdfConsumerFactory(){
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // Kafka broker adresi
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers); // Kafka broker adresi
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "pdf-consumer-group"); // Consumer group ID
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
